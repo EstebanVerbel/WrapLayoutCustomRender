a763eeef2212d600a81024dd23a87e85c5d2db45
KampusHub <verbelesteban2@gmail.com>
2017-01-04 19:33:07 -0500

    Added ObservableBaseObject class

---
+++ WrapLayoutCustomRender/WrapLayoutCustomRender/Models/Entities/ObservableBaseObject.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace WrapLayoutCustomRender.Models.Entities
+{
+    public class ObservableBaseObject : INotifyPropertyChanged
+    {
+
+        public ObservableBaseObject()
+        {
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string name = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
